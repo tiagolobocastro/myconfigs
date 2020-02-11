4ca94cbefc7dfdc416d5f06220629dc7d471c37e
tiago <tiagolobocastro@gmail.com>
2020-02-11 12:24:27 +0000

    Improve install script
    now we backup from a given fileList
    the same list is used to link/install
    
    todo: undo in case of failure - this is now possible since we have the
    fileList, simply add a trap in case any error occurrs during the link
    stage and undo the backups...
    but this is enough for now

--- install.sh
+++ install.sh
@@ -1,8 +1,48 @@
-# todo: backup existing!
+#!/bin/sh
 
-ln -s /home/tiago/git/myconfigs/.config/onedrive/config ~/.config/onedrive/
-ln -s ~/git/myconfigs/.vimrc ~/
-ln -s /home/tiago/git/myconfigs/.config/nvim ~/.config
-ln -s /home/tiago/git/myconfigs/.p10k.zsh ~/
-ln -s /home/tiago/git/myconfigs/.zshrc ~/
-ln -s /home/tiago/git/myconfigs/.oh-my-zsh/oh-my-zsh.sh ~/.oh-my-zsh # alternatively all: ln -s /home/tiago/git/myconfigs/.oh-my-zsh ~/
+# todo: undo in case of failure?
+
+BACKUP_LOCATION="~/.backups"
+SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# all paths must be within the user's home folder!
+backupOne() {
+  original_file="${1/#\~/$HOME}"
+  parent_dir=$( dirname "$original_file" )
+  parent_dir="${parent_dir/#\~/$HOME}"
+
+  if [ "$parent_dir" = "$(readlink -f ~)" ]; then
+    path_within_user=""
+  else
+    path_within_user=${parent_dir#~/}
+  fi
+
+  backup_path="$BACKUP_LOCATION"/"$path_within_user"
+  backup_path="${backup_path/#\~/$HOME}"
+
+  mkdir -p "$backup_path"
+  mv "$original_file" "$backup_path"
+}
+
+backup() {
+  for var in "$@"
+  do
+    backupOne "$var"
+  done
+}
+
+link() {
+  for var in "$@"
+  do
+    source_path="${var/#\~\/}"
+    destination_path="${var/#\~/$HOME}"
+    ln -s "$SCRIPT_LOCATION"/"$source_path" "$destination_path"
+  done
+}
+
+fileList=$(echo "~/.config/onedrive/config" "~/.vimrc" "~/.config/nvim" "~/.zshrc" "~/.p10k.zsh" "~/.oh-my-zsh/oh-my-zsh.sh")
+
+backup $fileList
+link $fileList
+
+echo "Installed successfully!"
