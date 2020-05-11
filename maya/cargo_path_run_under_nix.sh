7e370c9abbdf796ddb3f91d282bd5e5c45980a79
Tiago Castro <tiagolobocastro@gmail.com>
2020-05-11 23:41:57 +0100

    Add a very hacky script...
    ... which allows IDEA/VsCode to work properly by
    redirecting the cargo binaries into nix-shell (ouch!)

---
+++ maya/cargo_path_run_under_nix.sh
@@ -0,0 +1,36 @@
+# WARNING
+# This is a very hacky script which messes with your cargo install
+# allowing us to run cargo commands from nix within our own shell
+# and also allows IDEA/VsCode to run it's analyzis properly
+# ( todo: paths have been hardcoded which is not nice )
+# Run at your own risk - YOU HAVE BEEN WARNED
+
+create_bin() {
+cat << EOF > $1/$2
+#!/bin/bash
+run_nix_bin() {
+bin=\$(/usr/bin/which cargo)
+if [ \$bin = \$(readlink -f ~/.cargo/bin/cargo)" ]; then
+  ( ~/.nix-profile/bin/nix-shell --run "$2 \$*" ~/git/Mayastor/shell.nix )
+else
+    echo "Odd... Should not be running outside our shell.. exit"
+    exit 1
+fi
+}
+
+run_nix_bin \$@
+EOF
+chmod +x $1/$2
+}
+
+loc=$(mktemp -d)
+
+for f in ~/.cargo/bin/c*; do
+    create_bin $loc $(basename $f)
+done
+
+cp -rT ~/.cargo/bin_originals ~/.cargo/bin
+cp -rnT ~/.cargo/bin ~/.cargo/bin_originals
+cp -rT $loc ~/.cargo/bin
+
+rm -rf $loc
